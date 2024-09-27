422a4373fb3ab67722c2a10572bdad7f138f34e3
alvarowau <alvarobajo893@gmail.com>
2024-09-27 11:54:48 +0200

    Iniciación del proyecto

---
+++ airline-control/src/test/java/org/alvarowau/airlinecontrol/AirlineControlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvarowau.airlinecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
