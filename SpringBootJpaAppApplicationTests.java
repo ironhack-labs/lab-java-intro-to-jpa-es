d6f35c3b14d89e9ee859e2df857558b571bc6488
kmanurod <mrodcan1@gmail.com>
2024-11-12 13:15:46 +0100

    Add files via upload

---
+++ SpringBootJpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJpaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
